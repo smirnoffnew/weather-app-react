30eb810dafc81cf83cfc389930419506003cd0e2
smirnoffnew <smirnoffnew@gmail.com>
2020-10-30 17:46:32 +0200

    add: another api key

--- src/constants/apiKey.js
+++ src/constants/apiKey.js
@@ -1,1 +1,2 @@
-export const API_KEY = "27cd8a05f78f875dd237e1713d60bdfb";
+export const API_KEY = '27cd8a05f78f875dd237e1713d60bdfb'
+// this is a new api_key, in case an old expires: '087413f6edab51d45d3d73d594ea6431'
